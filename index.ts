28b5c76969a63745cb5201dc9b73b4d4bd7a1825
ZERHOUNI Ramzi <m.zerhouni@myskolae.fr>
2026-09-25 12:42:22 +0200

    Adding routes

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-const z = 10
+import figlet from "figlet"
 
-const read = (text: string) => {
-  return text
-}
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url)
 
-const msg = read("Hello World!")
-console.log(msg)
+    if (url.pathname === '/') {
+      const body = figlet.textSync("Hello !")
+      return new Response(body)
+    }
+    if (url.pathname === '/about') {
+      return new Response("About me")
+    }
+    if (url.pathname === '/feed') {
+      throw new Error('Could not fetch feed')
+    }
+    return new Response('404')
+  },
+  error(error) {
+    return new Response(`<pre> ${error} \n ${error.stack} </pre>`, {
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
+  }
+})
+
+console.log(`Listening on PORT http://localhost:${server.port}`)
